90f02394101932451c9e10e6a0b46adab140ca1a
Soulravager <alwinkuttikkattu@gmail.com>
2025-10-13 16:15:12 +0530

    Login-page 1

---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="flex-1 w-screen overflow-x-hidden">
+    <UserLogin />
+    <UserSignUP />
+  </main>
+</template>
+
+<script setup>
+import UserLogin from "../components/User/UserLogin.vue";
+import UserSignUP from "../components/User/UserSignUP.vue";
+</script>
